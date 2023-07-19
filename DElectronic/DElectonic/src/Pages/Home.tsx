9b58ec60d89141e8b9295466f7fc0e6d07c955e2
LuchezarStoymirov <luchostoimirov@gmail.com>
2023-07-19 23:46:05 +0300

    add animations to landing page

--- DElectronic/DElectonic/src/Pages/Home.tsx
+++ DElectronic/DElectonic/src/Pages/Home.tsx
@@ -1,14 +1,26 @@
 import style from "./Home.module.css";
+import { Parallax, ParallaxLayer } from "@react-spring/parallax";
 import { Header } from "../Components/Header/Header";
 import { NavBar } from "../Components/NavBar/NavBar";
 import { AboutUs } from "../Components/AboutUs/AboutUs";
+import { Services } from "../Components/Services/Services";
 
 export const Home = () => {
-    return(
-        <div className={style.container}>
-            <Header/>
-            <NavBar/>
-            <AboutUs/>
-        </div>
-    );
-}
+  return (
+    <Parallax pages={2.7}>
+      <div className={style.container}>
+        <ParallaxLayer>
+          <Header />
+          <NavBar />
+          <AboutUs />
+        </ParallaxLayer>
+        <ParallaxLayer offset={1} speed={0.5}>
+          <Services />
+        </ParallaxLayer>
+        <ParallaxLayer offset={1.9} speed={0.6}>
+          <Services />
+        </ParallaxLayer>
+      </div>
+    </Parallax>
+  );
+};
