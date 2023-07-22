961d52d4e651d743f83a322c1a20498f20485e5d
LuchezarStoymirov <luchostoimirov@gmail.com>
2023-07-22 09:18:46 +0300

    add navbar functionality

--- DElectronic/DElectonic/src/Components/NavBar/NavBar.tsx
+++ DElectronic/DElectonic/src/Components/NavBar/NavBar.tsx
@@ -1,21 +1,23 @@
 import { useState } from "react";
 import style from "./NavBar.module.css";
 
-export const NavBar = () => {
+export const NavBar = ({ parallaxRef }) => {
   const [activeButton, setActiveButton] = useState(null);
 
-  const handleButtonClick = (buttonIndex : any) => {
+  const handleButtonClick = (buttonIndex: any) => {
     setActiveButton(buttonIndex);
+
+    if (buttonIndex === 1) {
+      // Scroll to the second ParallaxLayer (offset={1.3})
+      parallaxRef.current.scrollTo(1.2);
+    } else if (buttonIndex === 2) {
+      // Scroll to the third ParallaxLayer (offset={2})
+      parallaxRef.current.scrollTo(2);
+    }
   };
 
   return (
     <div className={style.container}>
-      <h4
-        className={`${style.button} ${activeButton === 0 ? style.active : ""}`}
-        onClick={() => handleButtonClick(0)}
-      >
-        Начало
-      </h4>
       <h4
         className={`${style.button} ${activeButton === 1 ? style.active : ""}`}
         onClick={() => handleButtonClick(1)}
--- DElectronic/DElectonic/src/Pages/Home.tsx
+++ DElectronic/DElectonic/src/Pages/Home.tsx
@@ -4,20 +4,24 @@ import { Header } from "../Components/Header/Header";
 import { NavBar } from "../Components/NavBar/NavBar";
 import { AboutUs } from "../Components/AboutUs/AboutUs";
 import { Services } from "../Components/Services/Services";
+import { useRef } from "react";
 
 export const Home = () => {
+  const parallaxRef = useRef(); 
+
   return (
-    <Parallax pages={3}>
+    <Parallax ref={parallaxRef} pages={3}>
       <div className={style.container}>
         <ParallaxLayer>
           <Header />
-          <NavBar />
+          <NavBar parallaxRef={parallaxRef} />
           <AboutUs />
         </ParallaxLayer>
         <ParallaxLayer offset={1.3} speed={0.4}>
           <Services />
         </ParallaxLayer>
         <ParallaxLayer offset={2} speed={0.7}>
+          {/* Content for the third ParallaxLayer goes here */}
         </ParallaxLayer>
       </div>
     </Parallax>
